a6773954f8bf1846839f362ef80802f885d05e35
austinoboyle <14arob@queensu.ca>
2018-06-04 09:44:13 -0400

    Update package script file

--- misc/scripts/package.sh
+++ misc/scripts/package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf dist &&
 mkdir -p dist/client &&
-cp -r server/server.bundle.js server/views server/package.json dist &&
+cp -r server/views server/package.json dist &&
+mv server/server.bundle.js server/server.bundle.js.map dist &&
 cp -r client/build dist/client
