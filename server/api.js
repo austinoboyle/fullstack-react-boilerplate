b03f01943ce2d9f5c0ac40e219d16dee98571a00
austinoboyle <14arob@queensu.ca>
2018-06-11 16:10:52 -0400

    Add mongoose connection to api.js

--- server/api.js
+++ server/api.js
@@ -2,10 +2,15 @@ var express = require("express");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const todoRouter = require("./routers/todo.router");
 
 var app = express();
+
+mongoose
+    .connect("mongodb://localhost/todos")
+    .then(() => console.log("Connected to DB"))
+    .catch(e => console.error(e));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -14,6 +19,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/todo", todoRouter);
+if (process.env.NODE_ENV !== "production") {
+    app.use("/api/todos", todoRouter);
+} else {
+    app.use("/todos", todoRouter);
+}
 
 module.exports = app;
