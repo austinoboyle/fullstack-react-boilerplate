9a98e158de4e20e8b1fa28aa106a4e04df949b58
austinoboyle <14arob@queensu.ca>
2018-06-18 16:13:22 -0400

    Move TodoApp into Todos page

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
-import TodoApp from "./components/TodoApp/TodoApp";
+import Todos from "./components/pages/Todos/Todos";
 
 const routes = (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/todos" component={TodoApp} />
+        <Route path="/todos" component={Todos} />
     </Switch>
 );
 
